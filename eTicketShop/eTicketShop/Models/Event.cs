753646aa8fd95301eb80815ce5c02f9788868c19
Liutsiia <lusi_600@mail.ru>
2022-11-06 15:17:38 -0500

    Models

---
+++ eTicketShop/eTicketShop/Models/Event.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTicketShop.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Range(0.00, Double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Address { get; set; }
+
+        [StringLength(250)]
+        public string? Description { get; set; }
+
+        [StringLength(250)]
+        public string? ImageUrl { get; set; }
+
+        public Category Category { get; set; }
+        public ICollection<CartItem> CartItems { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
--- eTicketShop/eTicketShop/Program.cs
+++ eTicketShop/eTicketShop/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using eTicketShop.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TicketShopDB2ContextConnection") ?? throw new InvalidOperationException("Connection string 'TicketShopDB2ContextConnection' not found.");
+
+builder.Services.AddDbContext<TicketShopDB2Context>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<TicketShopDB2Context>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,11 +27,12 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
