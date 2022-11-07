4bc7c1c233cf96a11b0e637b3b8db3213c39e4f3
Liutsiia <lusi_600@mail.ru>
2022-11-06 21:58:37 -0500

    Roles

--- eTicketShop/eTicketShop/Program.cs
+++ eTicketShop/eTicketShop/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using eTicketShop.Areas.Identity.Data;
+using eTicketShop.Core;
+using eTicketShop.Core.Repositories;
+using eTicketShop.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("TicketShopDB2ContextConnection") ?? throw new InvalidOperationException("Connection string 'TicketShopDB2ContextConnection' not found.");
 
@@ -8,11 +12,19 @@ builder.Services.AddDbContext<TicketShopDB2Context>(options =>
     options.UseSqlServer(connectionString));
 
 builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<TicketShopDB2Context>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+#region Authorization
+
+AddAuthorizationPolicies();
+
+#endregion
+
+AddScoped();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -36,3 +48,24 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 app.Run();
+
+void AddAuthorizationPolicies()
+{
+    builder.Services.AddAuthorization(options =>
+    {
+        options.AddPolicy("EmployeeOnly", policy => policy.RequireClaim("EmployeeNumber"));
+    });
+
+    builder.Services.AddAuthorization(options =>
+    {
+        options.AddPolicy(Constants.Policies.RequireAdmin, policy => policy.RequireRole(Constants.Roles.Administrator));
+        options.AddPolicy(Constants.Policies.RequireManager, policy => policy.RequireRole(Constants.Roles.Manager));
+    });
+}
+
+void AddScoped()
+{
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+}
